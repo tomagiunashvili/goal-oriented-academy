5a35aaced69c97e338aacb12ea9810bc1dd6237a
tomagiunashvili <tomagiunashvili668@gmail.com>
2024-03-14 21:25:06 +0400

    commit

---
+++ day-10/lesson1.py
@@ -0,0 +1,12 @@
+question1 = bool(imput("type first answer (true  false)     "))
+question2 = bool(unput("tipe second answer     "))
+
+
+
+answer1 = question1 and question2
+answer2 = question1 or question2
+
+
+
+print(answer1)
+print(answer2)
